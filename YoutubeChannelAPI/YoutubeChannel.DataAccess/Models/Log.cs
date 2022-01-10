3a297a7b025660b85966d3415acd536f2cadd1db
Antonio <antonio.delagarza96@gmail.com>
2022-01-10 03:37:47 -0600

    New db context from AWS database

---
+++ YoutubeChannelAPI/YoutubeChannel.DataAccess/Models/Log.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace YoutubeChannel.DataAccess.Models
+{
+    public partial class Log
+    {
+        public int LogId { get; set; }
+        public string Action { get; set; }
+        public string Status { get; set; }
+        public string Message { get; set; }
+        public DateTime LogDate { get; set; }
+    }
+}
