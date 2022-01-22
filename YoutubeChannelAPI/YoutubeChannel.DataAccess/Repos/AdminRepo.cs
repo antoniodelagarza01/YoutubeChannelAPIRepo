d3e6209b7ec3b51214d4773185ac9472114b10d6
Antonio <antonio.delagarza96@gmail.com>
2022-01-22 15:45:11 -0600

    Added repo classes

---
+++ YoutubeChannelAPI/YoutubeChannel.DataAccess/Repos/AdminRepo.cs
@@ -0,0 +1,59 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using YoutubeChannel.DataAccess.Models;
+using System.Linq;
+
+namespace YoutubeChannel.DataAccess.Repos
+{
+    public class AdminRepo : IRepo<Admin>
+    {
+
+        private readonly YoutubeChannelDBContext context;
+
+        public AdminRepo(YoutubeChannelDBContext context)
+        {
+            this.context = context;
+        }
+        public bool Delete(int id)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_deleteAdmin_admin {0}", id));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<Admin> Get()
+        {
+            return context.Admins.ToList();
+        }
+
+        public Admin GetOne(int id)
+        {
+            return context.Admins.FirstOrDefault(x => x.AdminId == id);
+        }
+
+        public bool Post(Admin item)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_insertAdmin_admin {0}, {1}, {2}, {3}", item.FirstName, item.LastName, item.Email, item.Password));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Put(Admin item)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_updateAdmin_admin {0}, {1}, {2}, {3}", item.AdminId, item.FirstName, item.FirstName, item.Password));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ YoutubeChannelAPI/YoutubeChannel.DataAccess/Repos/VideoRepo.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using YoutubeChannel.DataAccess.Models;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+
+namespace YoutubeChannel.DataAccess.Repos
+{
+    public class VideoRepo : IRepo<Video>
+    {
+        private readonly YoutubeChannelDBContext context;
+
+        public VideoRepo(YoutubeChannelDBContext context)
+        {
+            this.context = context;
+        }
+        public bool Delete(int id)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_deleteVideo_videos {0}", id));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public IEnumerable<Video> Get()
+        {
+            return context.Videos.ToList();
+        }
+
+        public Video GetOne(int id)
+        {
+             return context.Videos.FirstOrDefault(x => x.VideoId == id);
+            
+        }
+
+        public bool Post(Video item)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_InsertVideo_videos {0}, {1}", item.Title, item.Media));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        public bool Put(Video item)
+        {
+            int result = Convert.ToInt32(context.Videos.FromSqlRaw("YT.sp_updateVideo_videos {0}", item.Title));
+            if (result == 1)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
